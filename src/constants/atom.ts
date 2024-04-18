a854e98c3a8035d741653ddc97c085001e13c1b9
iamfiro <appturbo102@gmail.com>
2024-04-18 21:51:28 +0900

    🔥 나중에 한번에 추가하게 atom.ts의 객체 삭제

--- src/constants/atom.ts
+++ src/constants/atom.ts
@@ -1,10 +1,4 @@
 import { AtomListType } from "../types";
 
 export const AtomList: AtomListType[] = [
-    {
-        name: {
-            ko: "수소",
-            en: "Hydrogen"
-        }
-    }
 ]
