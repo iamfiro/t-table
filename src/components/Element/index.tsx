8edbb86e4f29e7293706ab07835592c53ae98786
iamfiro <appturbo102@gmail.com>
2024-04-18 23:37:41 +0900

    ✨ 임시 컴포넌트 개발

---
+++ src/components/Element/index.tsx
@@ -0,0 +1,11 @@
+import style from './index.module.scss';
+
+const Element: React.FC = () => {
+    return (
+        <section className={style.container}>
+            안녕s하세요
+        </section>
+    )
+};
+
+export default Element;
